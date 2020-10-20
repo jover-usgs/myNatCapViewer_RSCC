b1dd8e7f9e8570798bb70b580d9b52d8921e9d3f
jover <jover@usgs.gov>
2020-10-20 11:06:57 -0400

    Updates

---
+++ precache-manifest.9e6b7539fb7359254f9b8eb1608d8e09.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285a9828d35a51e6d032665ac253ebb2",
+    "url": "/myNatCapViewer_RSCC/index.html"
+  },
+  {
+    "revision": "ef7ee4eb34416b74473b",
+    "url": "/myNatCapViewer_RSCC/static/css/2.0b5659ac.chunk.css"
+  },
+  {
+    "revision": "5d0ce5f68d89213f0378",
+    "url": "/myNatCapViewer_RSCC/static/css/main.dfa9379b.chunk.css"
+  },
+  {
+    "revision": "ef7ee4eb34416b74473b",
+    "url": "/myNatCapViewer_RSCC/static/js/2.ab97a2f6.chunk.js"
+  },
+  {
+    "revision": "11f86ddc566c30b6cad223d8a078311f",
+    "url": "/myNatCapViewer_RSCC/static/js/2.ab97a2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0ce5f68d89213f0378",
+    "url": "/myNatCapViewer_RSCC/static/js/main.df709685.chunk.js"
+  },
+  {
+    "revision": "f9f50252aeec2e1285a2",
+    "url": "/myNatCapViewer_RSCC/static/js/runtime-main.ec70031d.js"
+  },
+  {
+    "revision": "259d6ecf9142b73c4953cb2f36c24efb",
+    "url": "/myNatCapViewer_RSCC/static/media/ABBALegend.259d6ecf.png"
+  },
+  {
+    "revision": "c13fa0899ff1182ab56946d9f8d2463f",
+    "url": "/myNatCapViewer_RSCC/static/media/AOI.c13fa089.zip"
+  },
+  {
+    "revision": "6664085334b4fc5b97b19c05abbb48d7",
+    "url": "/myNatCapViewer_RSCC/static/media/Beijing.66640853.jpg"
+  },
+  {
+    "revision": "740829225a8ecb28422e6d31ca77332c",
+    "url": "/myNatCapViewer_RSCC/static/media/Final_RSCC_ROIs.74082922.zip"
+  },
+  {
+    "revision": "92a84ec95ef9d7a20ef1cbb42e7baa5c",
+    "url": "/myNatCapViewer_RSCC/static/media/Olympics.92a84ec9.jpg"
+  },
+  {
+    "revision": "50b7b0a7cc85ad6bee9c0fb40f8707ca",
+    "url": "/myNatCapViewer_RSCC/static/media/PeaIsland_GCPs.50b7b0a7.zip"
+  },
+  {
+    "revision": "f8041b0394cc02fa9256be370287c198",
+    "url": "/myNatCapViewer_RSCC/static/media/Watersheds.f8041b03.zip"
+  },
+  {
+    "revision": "a831c7165bb54c334bbed2ff05b76a89",
+    "url": "/myNatCapViewer_RSCC/static/media/af.a831c716.png"
+  },
+  {
+    "revision": "2e83c8d9cbf276e414501906dfa49020",
+    "url": "/myNatCapViewer_RSCC/static/media/airplane.2e83c8d9.svg"
+  },
+  {
+    "revision": "eefba0acd878390c34d3a6ad0ac1e134",
+    "url": "/myNatCapViewer_RSCC/static/media/bayBridge.eefba0ac.jpg"
+  },
+  {
+    "revision": "bcb375802e90be60db0b93da1a5d93e2",
+    "url": "/myNatCapViewer_RSCC/static/media/city.bcb37580.svg"
+  },
+  {
+    "revision": "32406ab8abb5edf5820cf907b0d1a799",
+    "url": "/myNatCapViewer_RSCC/static/media/cw.32406ab8.png"
+  },
+  {
+    "revision": "f76f7a39b93589b467d6356754e69d31",
+    "url": "/myNatCapViewer_RSCC/static/media/doubleLeaf.f76f7a39.svg"
+  },
+  {
+    "revision": "f9068c05277360aad1a5245e86e3aa2a",
+    "url": "/myNatCapViewer_RSCC/static/media/exampleLegend4.f9068c05.png"
+  },
+  {
+    "revision": "19a7859f33b2bd302f2dcc1ac3ffeeca",
+    "url": "/myNatCapViewer_RSCC/static/media/exampleRaster.19a7859f.tif"
+  },
+  {
+    "revision": "002da6880945ede5f708f363945eb8ea",
+    "url": "/myNatCapViewer_RSCC/static/media/forest.002da688.svg"
+  },
+  {
+    "revision": "137612a416f30b34f59ad10dc2d81f0b",
+    "url": "/myNatCapViewer_RSCC/static/media/halloween.137612a4.jpg"
+  },
+  {
+    "revision": "b1b2edf8be2c33bf750558378bda5ed8",
+    "url": "/myNatCapViewer_RSCC/static/media/headerImg.b1b2edf8.png"
+  },
+  {
+    "revision": "18bb21a79bc8d8119a8063a01c34bb7a",
+    "url": "/myNatCapViewer_RSCC/static/media/herrin.18bb21a7.jpg"
+  },
+  {
+    "revision": "a5b34d779888efa0a6be87366602764b",
+    "url": "/myNatCapViewer_RSCC/static/media/house.a5b34d77.svg"
+  },
+  {
+    "revision": "1ee2cc6af91d717b9fc99ce9e8af8880",
+    "url": "/myNatCapViewer_RSCC/static/media/leaf.1ee2cc6a.svg"
+  },
+  {
+    "revision": "087bbae76bbc32a657f792dd3c66498a",
+    "url": "/myNatCapViewer_RSCC/static/media/minneapolis.087bbae7.jpg"
+  },
+  {
+    "revision": "1053ed6f79c0f1fb66402d40cb45e8c7",
+    "url": "/myNatCapViewer_RSCC/static/media/natCapAdventures.1053ed6f.zip"
+  },
+  {
+    "revision": "c6a9c80e61161204827462d8e1213897",
+    "url": "/myNatCapViewer_RSCC/static/media/natcap.c6a9c80e.png"
+  },
+  {
+    "revision": "8ac0faef57d3116b718290f220060876",
+    "url": "/myNatCapViewer_RSCC/static/media/nationalParks.8ac0faef.zip"
+  },
+  {
+    "revision": "c2ce20e8789eac9eb06bff205a02bb51",
+    "url": "/myNatCapViewer_RSCC/static/media/no-profile-img.c2ce20e8.png"
+  },
+  {
+    "revision": "80b85c0259678788d670c1b61c38b5eb",
+    "url": "/myNatCapViewer_RSCC/static/media/norrkoping.80b85c02.png"
+  },
+  {
+    "revision": "2e23d06d99569744b010926cc623c9b9",
+    "url": "/myNatCapViewer_RSCC/static/media/puertoMaldonado.2e23d06d.jpg"
+  },
+  {
+    "revision": "d2249e85847a392570312e6aae1c5f47",
+    "url": "/myNatCapViewer_RSCC/static/media/redmond.d2249e85.jpg"
+  },
+  {
+    "revision": "a2778aa91b27776c6e1d041197904b46",
+    "url": "/myNatCapViewer_RSCC/static/media/stockholm.a2778aa9.jpg"
+  },
+  {
+    "revision": "e734bea51caca1f902c41a483ff793e5",
+    "url": "/myNatCapViewer_RSCC/static/media/wyoming.e734bea5.jpg"
+  }
+]);
